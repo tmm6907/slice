1dd68ae720b899ad9af4049ee28416d5f57435b4
tmm6907 <tmm6907@gmail.com>
2025-11-21 03:21:23 -0500

    version 1.0.0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tmm6907/slice
+
+go 1.25.3
---
+++ slice.go
@@ -0,0 +1,32 @@
+package main
+
+// Map applies a transformation function to every element of a slice T and returns a new slice of V.
+func Map[T, V any](s []T, transform func(t T) V) []V {
+	res := make([]V, len(s))
+	for i, v := range s {
+		res[i] = transform(v)
+	}
+	return res
+}
+
+// Filter iterates over a slice T and returns a new slice containing only the elements
+// for which the provided filter function returns true.
+func Filter[T any](s []T, filter func(t T) bool) []T {
+	res := make([]T, 0, len(s))
+	for _, v := range s {
+		if filter(v) {
+			res = append(res, v)
+		}
+	}
+	return res
+}
+
+// Reduce combines all elements of a slice T into a single accumulated value V,
+// starting with an initial value.
+func Reduce[T, V any](s []T, initial V, reduce func(acc V, curr T) V) V {
+	res := initial
+	for _, v := range s {
+		res = reduce(res, v)
+	}
+	return res
+}
